bdb6d4f9033d58424dc631313d60f2bd2c1e6972
gdcnb <4013531@qq.com>
2013-11-03 19:11:38 +0800

    microblog

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
+};
+
+exports.user = function(req, res){
+
+};
+exports.post = function(req, res){
+
+};
+exports.reg = function(req, res){
+
+};
+exports.login = function(req, res){
+
+};
+exports.doLogin = function(req, res){
+
+};
+exports.logout = function(req, res){
+
+};
